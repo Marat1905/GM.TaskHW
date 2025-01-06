50d566a7ba8db87ed2c00e9a4545260a9ce95594
Marat <milya010790@mail.ru>
2025-01-06 19:15:06 +0500

    Выполнена работа

--- GM.TaskHW/Program.cs
+++ GM.TaskHW/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace GM.TaskHW
+﻿using System.Diagnostics;
+
+namespace GM.TaskHW
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string testFilesDir = Path.GetFullPath(@"..\..\..\restFiles");
+
+            Console.WriteLine($"Поиск файлов в папке:  {testFilesDir}");
+            Console.WriteLine();
+
+            var stopwatch = Stopwatch.StartNew();
+            try
+            {
+                int totalSpaces = await TextAnalyzer.CountSpacesInDirectoryAsync(testFilesDir);
+                stopwatch.Stop();
+                Console.WriteLine($"Всего пробелов: {totalSpaces}");
+                Console.WriteLine($"Затраченное время: {stopwatch.ElapsedMilliseconds} ms");
+            }
+           
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch ( DirectoryNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception) 
+            {
+                Console.WriteLine("Не предвиденная ошибка");
+            }
+           
         }
     }
 }
---
+++ GM.TaskHW/TextAnalyzer.cs
@@ -0,0 +1,32 @@
+﻿namespace GM.TaskHW
+{
+    public static class TextAnalyzer
+    {
+        /// <summary>Подсчет всех пробелов в каталоге </summary>
+        /// <param name="directoryPath">Путь к каталогу</param>
+        /// <returns>Возвращаем количество пробелов</returns>
+        public static async Task<int> CountSpacesInDirectoryAsync(string directoryPath)
+        {
+            if (String.IsNullOrEmpty(directoryPath))
+                throw new ArgumentException("Путь не указан.");
+            if (!Directory.Exists(directoryPath))
+                throw new DirectoryNotFoundException("Попытка доступа к пути, отсутствующему на диске.");
+
+            var files = Directory.GetFiles(directoryPath);         
+            var tasks = files.Select(CountSpacesInFileAsync);
+            var results = await Task.WhenAll(tasks);
+
+            return results.Sum();
+        }
+
+        /// <summary>Метод для подсчета количество пробелов в файле</summary>
+        /// <param name="filePath">Путь к файлу</param>
+        /// <returns>Подсчет количества пробелов</returns>
+        private static async Task<int> CountSpacesInFileAsync(string filePath)
+        {
+            string content = await File.ReadAllTextAsync(filePath);
+
+            return content.Count(char.IsWhiteSpace);
+        }
+    }
+}
